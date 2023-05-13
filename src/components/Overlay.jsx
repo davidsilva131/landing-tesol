f1ace042b49a42e0a35e9ef1929e09f07d365154
David Silva <davidsilvac131@gmail.com>
2023-05-13 13:12:27 -0500

    Fix link to instagram

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 import tesolLogo from '/public/favicon.ico'
 const Overlay = () => {
   return (
-    <div className='absolute top-0 left-0 pointer-events-none w-full h-full'>
+    <div className='absolute top-0 left-0 w-full h-full'>
       <Image className='absolute top-5 left-10 rounded-full' src={tesolLogo} width={75} height={75} alt='Logo Tejido solidario' />
       <span className='absolute top-9 left-36 text-sm'>
         Tejido
         <br />
         Solidario
       </span>
-      <Link className='absolute top-9 right-10 pointer-events-auto' href='https://www.instagram.com/solidariotesol/' target='_blank' alt='Link al instagram de tejido solidario'>
+      <Link className='absolute top-9 right-10 z-10' href='https://www.instagram.com/solidariotesol/' target='_blank' alt='Link al instagram de tejido solidario'>
         <Image src='https://icongr.am/fontawesome/instagram.svg?size=30&color=ffffff' width={30} height={30} alt='Logo instagram' />
       </Link>
     </div>
