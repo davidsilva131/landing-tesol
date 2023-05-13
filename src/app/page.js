123c0b5e7278f0d0b790d4465baad981d0686c2f
David Silva <davidsilvac131@gmail.com>
2023-05-13 11:36:29 -0500

    Add componento to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+'use client'
+import { Canvas } from '@react-three/fiber'
+import { state } from '@/Utils/threeUtils'
+import Items from '@/components/Items'
 
 export default function Home () {
   return (
-    <main>
-      <h1>home</h1>
-    </main>
+    <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} onPointerMissed={() => (state.clicked = null)}>
+      <Items />
+    </Canvas>
   )
 }
